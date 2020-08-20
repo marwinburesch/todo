4d66d22b6c61b1f4fa7138a5de4643747032163d
marwinburesch <marwin@posteo.de>
2020-08-20 16:24:29 +0200

    Create GlobalStyles and move css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AddTodo from "./pages/AddTodo";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/add">
-          <AddTodo />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route path="/add">
+            <AddTodo />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
